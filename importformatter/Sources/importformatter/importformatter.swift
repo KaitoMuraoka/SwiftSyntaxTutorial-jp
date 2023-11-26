969b8f96c1e2d6a5f4e71ad4e48cc9d1cf0faf84
KaitoMuraoka <kaitoboshi@icloud.com>
2023-11-26 15:56:08 +0900

    finish Tutorial

--- importformatter/Sources/importformatter/importformatter.swift
+++ importformatter/Sources/importformatter/importformatter.swift
@@ -1,2 +1,78 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftSyntax
+import SwiftParser
+import Foundation
+
+@main struct importformatter {
+    static func main() {
+        guard CommandLine.arguments.count == 2 else {
+            print("Not enought arguments!")
+            return
+        }
+        
+        let filePath = CommandLine.arguments[1]
+        guard FileManager.default.fileExists(atPath: filePath) else {
+            print("File doesn't exist at path: \(filePath)")
+            return
+        }
+        
+        guard let file = try? String(contentsOfFile: filePath) else {
+            print("File at path isn't readable: \(filePath)")
+            return
+        }
+        
+        let formattedFile = importformatter().formatImports(in: file)
+        print(formattedFile)
+    }
+    
+    func formatImports(in file: String) -> SourceFileSyntax {
+        let sourceFile = Parser.parse(source: file)
+        var items = classifyItems(in: sourceFile)
+        let pivotPoint = items.partition { item in
+            switch item {
+            case .import(_, _):
+                return false
+            case .other(_):
+                return true
+            }
+        }
+        items[..<pivotPoint].sort { lhs, rhs in
+            guard
+                case let .import(lhsImport, _) = lhs,
+                case let .import(rhsImport, _) = rhs
+            else {
+                fatalError("Partition must only contain import items!")
+            }
+            return lhsImport.path.description < rhsImport.path.description
+        }
+        let formattedStatements = items.enumerated().map { (offset, item) in
+            switch item {
+            case .import(_, let statement):
+                return statement
+                    .with(\.leadingTrivia, offset == 0 ? [] : .newline)
+                    .with(\.trailingTrivia, [])
+            case .other(let statement):
+                return statement
+            }
+        }
+        return sourceFile
+            .with(\.statements, CodeBlockItemListSyntax(formattedStatements))
+    }
+    
+    enum Item {
+        case `import` (ImportDeclSyntax, CodeBlockItemSyntax)
+        case other(CodeBlockItemSyntax)
+    }
+    
+    func classifyItems(in file: SourceFileSyntax) -> [Item] {
+        file.statements.map { statement in
+            if case .decl(let decl) = statement.item,
+               let `import` = decl.as(ImportDeclSyntax.self) {
+                return .import(`import`, statement)
+            } else {
+                return .other(statement)
+            }
+        }
+    }
+}
